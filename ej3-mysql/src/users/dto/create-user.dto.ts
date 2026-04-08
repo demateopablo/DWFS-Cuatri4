5d7f117aaf17141b349c08e80c2963b373f51b78
Pablo Demateo <pablodem32@gmail.com>
2026-04-08 20:35:53 -0300

    Refactor: Eliminar el uso de "!" en las propiedades de CreateUserDto para mejorar la claridad del código. Se resuelve agregando un constructor simple para cada atributo.

--- ej3-mysql/src/users/dto/create-user.dto.ts
+++ ej3-mysql/src/users/dto/create-user.dto.ts
@@ -1,5 +1,11 @@
 export class CreateUserDto {
-  name!: string;
-  age!: number;
-  email!: string;
+  name: string;
+  age: number;
+  email: string;
+
+  constructor(name: string, age: number, email: string) {
+    this.name = name;
+    this.age = age;
+    this.email = email;
+  }
 }
