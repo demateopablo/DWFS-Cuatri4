27368fb62864450c51202de603d7efd4488b2c6b
Pablo Demateo <pablodem32@gmail.com>
2026-04-08 20:11:54 -0300

    Refactor: Simplificar el acceso a las variables de entorno en app.module.ts

--- ej3-mysql/src/app.module.ts
+++ ej3-mysql/src/app.module.ts
@@ -3,15 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 
+const env = process.env;
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
       type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT ?? '3306'),
-      username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || 'root',
-      database: process.env.DB_NAME || 'ej3_db',
+      host: env.DB_HOST || 'localhost',
+      port: parseInt(env.DB_PORT ?? '3306', 10),
+      username: env.DB_USER || 'root',
+      password: env.DB_PASSWORD || 'root',
+      database: env.DB_NAME || 'ej3_db',
       entities: [User],
       synchronize: true,
     }),
