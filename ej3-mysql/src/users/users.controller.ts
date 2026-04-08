667e6ec759530b7bf7625bb367716a5b242c9fda
Pablo Demateo <pablodem32@gmail.com>
2026-04-08 20:12:20 -0300

    Refactor: Simplificar las rutas en UsersController (se asigna la ruta directo en el decorador)

--- ej3-mysql/src/users/users.controller.ts
+++ ej3-mysql/src/users/users.controller.ts
@@ -11,31 +11,31 @@ import {
 import { UsersService } from './users.service';
 import { User } from './user.entity';
 
-@Controller()
+@Controller('/users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Get('/users')
+  @Get()
   async getUsers(): Promise<User[]> {
     return this.usersService.findAll();
   }
 
-  @Get('/users/:id')
+  @Get(':id')
   async getUser(@Param('id') id: string): Promise<User | null> {
     return this.usersService.findOne(parseInt(id));
   }
 
-  @Post('/users')
+  @Post('')
   postUser(@Body() newUser: User): Promise<User> {
     return this.usersService.postUser(newUser);
   }
 
-  @Delete('/users/:id')
+  @Delete(':id')
   async deleteUser(@Param('id') id: string): Promise<void> {
     return await this.usersService.remove(parseInt(id));
   }
 
-  @Put('/users/:id') //Full update
+  @Put(':id') //Full update
   updateUser(
     @Param('id') id: string,
     @Body() updatedUser: User,
@@ -43,7 +43,7 @@ export class UsersController {
     return this.usersService.updateUser(parseInt(id), updatedUser);
   }
 
-  @Patch('/users/:id') //Partial update
+  @Patch(':id') //Partial update
   patchUser(
     @Param('id') id: string,
     @Body() updatedUser: Partial<User>,
